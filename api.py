66943ff91a7ad262eb2020cc99cd05b5273f18c0
Ronnie <ronnieshum1996@gmail.com>
2018-10-22 16:39:09 -0400

    Modify api params

--- api.py
+++ api.py
@@ -7,15 +7,13 @@ gcloud_api_key=os.environ["GCLOUD_KEY"]
 context = ssl._create_unverified_context()
 google_api_root="https://maps.googleapis.com/maps/api/"
 
-
-
-def get_distance_matrix(origin, destination):
+def get_distance_matrix(origin, destination, units="metric", languages="en"):
     params = [
-        ("units","metric"),
+        ("units",units),
         ("origins", origin),
         ("destinations", destination),
         ("key", gcloud_api_key),
-        ("language", "en")
+        ("language", languages)
     ]
     url = google_api_root + "distancematrix/json?" + urllib.parse.urlencode(params)
     contents = urllib.request.urlopen(url, context=context).read()
