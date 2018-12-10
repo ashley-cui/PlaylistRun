2afe2b24e038948ee6832c0a9a5d89aac8497285
Ashley Cui <ashleycui16@gmail.com>
2018-12-10 16:28:24 -0500

    revert routing to simple auth

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, request, Response, render_template,redirect,session
+from flask import Flask, request, Response, render_template,redirect
 import os.path
 import api
 import spotauth
-
-
 
 def root_dir():  # pragma: no cover
     return os.path.abspath(os.path.dirname(__file__))
@@ -22,7 +20,7 @@ def get_file(filename):  # pragma: no cover
 
 
 app = Flask(__name__)
-app.secret_key="hi"
+
 @app.route('/')
 def index():
     content = get_file('static/index.html')
@@ -53,32 +51,6 @@ def api_demo_result():
     duration = response['rows'][0]['elements'][0]['duration']['text']
 
     return render_template('result.html',origin=origin_formatted, destination=destination_formatted, distance=distance, duration=duration)
-
-@app.route('/login')
-def login():
-    if 'username' in session:  #check if session is alive 
-        username = session['username']
-        return 'Logged in as ' + username + '<br>' + \
-         "<b><a href = '/logout'>click here to log out</a></b>"
-
-
-    rend=get_file('static/login.html')
-    return Response(rend, mimetype="text/html")
-
-@app.route('/login/result',methods=['POST'])
-def loginr():
-    name = request.form['name'] #get username from form
-    session['username']=name #create session with username
-
-    return 'Logged in as ' + session['username'] + '<br>' + \
-         "<b><a href = '/logout'>click here to log out</a></b>"
-@app.route('/logout')
-def logout():
-   # end session
-   session.pop('username', None)
-   return redirect('/login')
-
-
 @app.route('/spot')
 def spot():
     signin=spotauth.auth()
@@ -88,13 +60,6 @@ def callback():
     r=spotauth.songs(request.url)
 
     return render_template('authsuccess.html',stuff=r)
-
-
-
-
-
-
-
 
 
 
